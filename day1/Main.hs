873b1c2be6ee6f07c0fa9ad185eb27b2a8811172
Joris Burgers <jorisburgers@live.nl>
2021-12-11 20:20:34 +0100

    Add Day 1 part 2

--- day1/Main.hs
+++ day1/Main.hs
@@ -4,19 +4,24 @@
 newtype Depth = Depth {unDepth :: Integer}
   deriving (Eq, Ord, Show)
 
-depthIncrease :: [Depth] -> Integer
-depthIncrease = go 0
+depthSliceValue :: [Depth] -> Depth
+depthSliceValue = Depth . sum . map unDepth 
+
+depthIncrease :: Int -> [Depth] -> Integer
+depthIncrease compareSize = go 0
   where
     go total [] = total
-    go total (m:[]) = total
-    go total (x:y:ys) 
-      | x < y = go (total + 1) (y:ys)
-      | otherwise = go total (y:ys)
+    go total ms | length (take compareSize ms) < compareSize = total
+    go total ms =
+      let curSlice = depthSliceValue (take compareSize ms)
+          nextSlice = depthSliceValue (take compareSize (drop 1 ms))
+          newTotal = if curSlice < nextSlice then total + 1 else total
+      in go newTotal (drop 1 ms)
 
 main :: IO ()
 main = do
   let inputFile = "input"
   lines <- lines <$> readFile inputFile
   let depths = map (Depth . read) lines
-  print (depthIncrease depths)
+  print (depthIncrease 3 depths)
 

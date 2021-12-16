3053cfa1adaab129cc27d64d0deb1760c46b9947
Joris Burgers <jorisburgers@live.nl>
2021-12-17 00:20:56 +0100

    Add Day 6 Part 2

--- day6/Main.hs
+++ day6/Main.hs
@@ -1,6 +1,8 @@
 import Text.Parsec
 import Text.Parsec.String
 
+import qualified Data.Map as M
+
 pFishes :: Parser [Int]
 pFishes = sepBy pFish (char ',') <* eof
 
@@ -13,17 +15,21 @@ newFish = 8
 resetFish :: Int
 resetFish = 6
 
-iteration :: [Int] -> [Int]
-iteration = go []
-  where 
-    go new [] = new
-    go new (x:xs) 
-      | x == 0 = resetFish : go (newFish:new) xs
-      | otherwise = (x - 1) : go new xs
+construct :: [Int] -> M.Map Int Int
+construct = foldr (\x -> M.insertWith (+) x 1) M.empty
 
-iterations :: [Int] -> Int -> [Int]
-iterations list 0 = list
-iterations list n = iterations (iteration list) (n - 1)
+iteration :: M.Map Int Int -> M.Map Int Int 
+iteration m = 
+  let newFishes = maybe 0 id (M.lookup 0 m) 
+  in M.insert newFish newFishes (M.mapKeysWith (+) bumpFish m)
+  where 
+    bumpFish :: Int -> Int
+    bumpFish 0 = resetFish
+    bumpFish n = n - 1
+
+iterations :: M.Map Int Int -> Int -> M.Map Int Int
+iterations m 0 = m
+iterations m n = iterations (iteration m) (n - 1)
 
 main :: IO ()
 main = do
@@ -32,4 +38,5 @@ main = do
   case res of 
     Left e -> print e
     Right fishes -> do
-      print (length $ iterations fishes 80)
+      let initial = construct fishes
+      print (M.foldr (+) 0 $ iterations initial 256)

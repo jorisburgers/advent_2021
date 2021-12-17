d7da096aee8e0879a590d78a3df4ed7321982946
Joris Burgers <jorisburgers@live.nl>
2021-12-18 00:28:34 +0100

    Add Day 8 Part 2

--- day8/Main.hs
+++ day8/Main.hs
@@ -1,6 +1,8 @@
 import Text.Parsec hiding (Line)
 import Text.Parsec.String
 
+import Data.List
+
 import qualified Data.Map as M
 
 data Line = Line [String] [String]
@@ -18,8 +20,54 @@ pLine = Line <$> pSegments <* string "| " <*> pSegments
 pLines :: Parser [Line]
 pLines = sepBy pLine endOfLine <* eof
 
-countSndLengths :: [Int] -> Line -> Int
-countSndLengths ls (Line _ xs) = length (filter (\x -> length x `elem` ls) xs)
+decode :: M.Map String Int -> String -> Int
+decode table s = maybe (error ("Unkown string " <> s <> " " <> show table )) id (M.lookup (sort s) table)
+
+truthTable :: [String] -> M.Map Int String  
+truthTable xs = 
+  let 
+    xs' = xs <> xs <> xs
+    go :: M.Map Int String  -> [String] -> M.Map Int String 
+    go m [] = error (show m)
+    go m (x:xs) 
+      | M.size m == 10 = m
+      | otherwise =
+          go (maybe m (\v -> M.insert v x m) (decipher m x)) xs
+  in go M.empty xs' 
+
+decipher :: M.Map Int String -> String -> Maybe Int
+decipher m x 
+  | length x == 2 = pure 1
+  | length x == 3 = pure 7
+  | length x == 4 = pure 4
+  | length x == 7 = pure 8
+  | length x == 5 = do
+      x4 <- M.lookup 4 m
+      x1 <- M.lookup 1 m
+      if x `union` x1 == x then
+        pure 3
+      else do
+        x9 <- M.lookup 9 m
+        if x `intersect` x9 == x then
+          pure 5
+        else
+          pure 2
+  | length x == 6 = do
+      x4 <- M.lookup 4 m
+      x1 <- M.lookup 1 m
+      if x `union ` x1 `union ` x4 == x then
+        pure 9
+      else if x `union` x1 == x then
+        pure 0
+      else 
+        pure 6
+  | otherwise = error (show (m, x))
+
+decodeLine :: Line -> Int
+decodeLine (Line uniques outputs) = 
+  let 
+    table = M.fromList $ map (\(x, y) -> (sort y, x)) $ M.toList $ truthTable uniques
+  in foldl' (\acc x -> acc * 10 + x) 0 $ map (decode table) outputs
 
 main :: IO ()
 main = do
@@ -28,4 +76,5 @@ main = do
   case res of 
     Left e -> print e
     Right lines -> do
-      print (sum (map (countSndLengths [2, 3, 4, 7]) lines))
+      print (sum $ map decodeLine lines)
+      

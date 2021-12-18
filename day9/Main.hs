52724270ea3cf57994a96fcf183b600bc706bbbc
Joris Burgers <jorisburgers@live.nl>
2021-12-18 21:17:15 +0100

    Add Day 9 Part 2

--- day9/Main.hs
+++ day9/Main.hs
@@ -2,6 +2,7 @@ import Text.Parsec hiding (Line)
 import Text.Parsec.String
 
 import Data.Maybe (mapMaybe)
+import Data.List (sort)
 
 import qualified Data.Map as M
 
@@ -17,11 +18,23 @@ pInt = read . (:[]) <$> digit
 constructAreaMap :: [[Int]] -> M.Map (Int, Int) Int
 constructAreaMap xs = M.fromList $ concat $ zipWith (\line y -> zipWith (\height x -> ((x, y), height)) line [0..]) xs [0..]
 
+neighboursCoords :: (Int, Int) -> [(Int, Int)]
+neighboursCoords (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
 neighbours :: M.Map (Int, Int) Int -> (Int, Int) -> [Int]
-neighbours area (x, y) = mapMaybe (flip M.lookup area) [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+neighbours area pos = mapMaybe (flip M.lookup area) (neighboursCoords pos)
 
 lowestPoints :: M.Map (Int, Int) Int -> M.Map (Int, Int) Int
 lowestPoints area = M.filterWithKey (\pos height -> all (> height) (neighbours area pos)) area
+
+basin :: M.Map (Int, Int) Int -> (Int, Int) -> M.Map (Int, Int) Int
+basin area pos = go M.empty pos
+  where
+    go curBasin curPos 
+      | not (curPos `M.member` area) = M.empty
+      | M.lookup curPos area == Just 9 = M.empty
+      | curPos `M.member` curBasin = M.empty
+      | otherwise = foldr (\nPos b -> M.union b (go b nPos)) (M.insert curPos (area M.! curPos) curBasin) (neighboursCoords curPos)
 
 main :: IO ()
 main = do
@@ -30,5 +43,7 @@ main = do
   case res of 
     Left e -> print e
     Right area -> do
-      print (sum $ M.map (+1) $ lowestPoints $ constructAreaMap area)
+      let areaMap = constructAreaMap area
+      let basins = map (basin areaMap) $ M.keys (lowestPoints areaMap)
+      print (product $ take 3 $ reverse $ sort $ map (\b -> M.size b) basins)
       
